72dc60ed602618a314a96511f29deaad012d8c7d
georgebrian7 <brianmecha021@gmail.com>
2026-07-30 21:28:21 +0300

    added server and urls

--- app.js
+++ app.js
@@ -3,15 +3,70 @@ const fs = require('fs');
 
 const http =require('http');
 const server = http.createServer((req, res)=>{
-    if (req.url ==='/'){
-        res.write('Hello World');
-        res.end();
+     res.setHeader('Content-Type','text/html');
+
+
+    let path ='./';
+    switch(req.url){
+        case'/home':
+            path += 'home.html';
+            res.statusCode =200;
+            break;
+        case'/about':
+            path += 'about.html';
+            res.statusCode =200;
+            break;
+        default:
+            path += '404.html';
+            res.statusCode =400;
+            break;
+        
     }
+
+
+
+     fs.readFile(path,(err,data)=>{
+        if(err){
+            console.log('errorrrrr');
+            res.end(); 
+        }else{
+            res.write(data);
+            res.end();
+        }
+     })
+    
 });
 
 server.on('connection', (socket)=>{
     console.log('new connection');
 });
-server.listen(3000);
+server.listen(3000, 'localhost' ,()=>{
+    console.log('listening on port 3000')
+});
 
-console.log('Listening on port 3000')
+// console.log('Listening on port 3000')
+// fs.readFile('./joke.txt',(err, data) =>{
+//     if(err) {
+//         console.log(err);
+//     }
+//     console.log(data.toString());
+    
+// });
+// fs.writeFile('./joke.txt', 'LOL' ,(data) =>{
+//     console.log(data)
+// // })
+// if(!fs.existsSync('./assets')){
+// fs.mkdir('./assets' , (err) =>{
+// if(err){
+//     console.log(err);
+// }
+// console.log('folder created')
+// });
+// } else{
+//     fs.rmdir('./assets',(err) =>{
+//         if(err){
+//             console.log(err)
+//         }
+//         console.log('folder deleted');
+//     })
+// }
