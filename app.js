2bf0f8c156a26d68a32b5881a7b3dafe78513d41
georgebrian7 <brianmecha021@gmail.com>
2026-08-03 18:05:12 +0300

    added mongodb connection and routes

--- app.js
+++ app.js
@@ -1,21 +1,45 @@
 const express = require('express');
 const morgan = require('morgan');
 const mongoose =require('mongoose');
-
+const Blog =require('./models/blog')
 const app = express();
 
-const dbURI ="mongodb+srv://trey:<PRq1MZexZNV33RNd>@cluster0.abzjfqb.mongodb.net/?appName=Cluster0"
+const dbURI ="mongodb+srv://trey:PRq1MZexZNV33RNd@cluster0.abzjfqb.mongodb.net/?appName=Cluster0"
 mongoose.connect(dbURI)
+    .then((result)=> app.listen(3000))
     .then((result)=> console.log('connected to db'))
     .catch((err) => console.log(err));
 app.set('view engine', 'ejs');
-app.listen(3000);
+
 
 
 app.use(express.static('public'));
 app.use(morgan('dev'));
 
 
+app.get('/add-blog', (req,res)=>{
+    const blog =new Blog({
+        title:'narnia 2',
+        snippet:'good place',
+        body:'a long time in narnia'
+    });
+    blog.save()
+        .then((result)=>{
+            res.send(result)
+        })
+        .catch((err)=>{
+            console.log(err);
+        });
+});
+app.get('/all-blogs', (req,res)=>{
+    Blog.find()
+        .then((result)=>{
+            res.send(result)
+        })
+        .catch((err)=>{
+            console.log(err);
+        });
+});
 
 
 app.get('/', (req,res)=>{
